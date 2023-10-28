bb411f5e6137ac3b75a736925ca5222c71404f26
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-28 20:02:29 -0300

    set the basic template for about page

--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Finch Collector</h1>')
+
+def about(request):
+  return render(request,'about.html')
