252df5056d90e69b4339090afa404360031f0075
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-29 02:54:21 -0300

    complete finch-index function and route the url

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('',views.home,name='home'),
-  path('about/',views.about,name='about')
+  path('about/',views.about,name='about'),
+  path('finches/',views.finch_index,name='finch-index'),
   ] 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+class Finch:
+  def __init__(self,name,breed,description,age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+finches =[
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def finch_index(request):
+  return render(request,'finches/index.html',{'finches':finches})
+
 def home(request):
   return HttpResponse('<h1>Finch Collector</h1>')
 
