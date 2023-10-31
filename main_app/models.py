efe989af177d4c013d2aa3bb4c995e3cf025fd06
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-10-31 00:53:27 -0300

    set the feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -8,9 +8,25 @@ class Finch(models.Model):
   description = models.TextField(max_length=250)
   age = models.IntegerField()
 
+MEALS = (
+  ('B','Breakfast'),
+  ('L','Lunch'),
+  ('D','Dinner'),
+  ('S','Snack')
+)
+class Feeding(models.Model):
+  date_and_time = models.DateTimeField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+
   def __str__(self):
     return self.name
   
   def get_absolute_url(self):
       return reverse("finch-detail", kwargs={"finch_id": self.id})
   
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date_and_time} ' 
