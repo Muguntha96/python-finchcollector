3c044da3612bc98e581b919d792940385caf530f
Muguntha96 <muguntharamkumar1996@gmail.com>
2023-11-02 15:27:37 -0300

    add path and nav bar link for  toy

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
   path('finches/<int:pk>/delete',views.FinchDelete.as_view(),name='finch-delete'),
   path('finches/<int:finch_id>/add-feeding/', views.add_feeding, name='add-feeding'),
   path('toys/create/',views.ToyCreate.as_view(),name='toy-create'),
-  path('')
+  path('toys/<int:pk>/',views.ToyDetail.as_view(),name='toy-detail'),
+  path('toys/', views.ToyList.as_view(), name='toy-index'),
   
   ] 
